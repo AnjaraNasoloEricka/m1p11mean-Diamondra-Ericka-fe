536e84aef8272957d4c04fbd7bdbd97f802be2cf
d.rambeloson <d.rambeloson@etechconsulting-mg.com>
2024-02-28 16:36:20 +0300

    [feat] update employee

--- src/app/model/Employees.ts
+++ src/app/model/Employees.ts
@@ -1,6 +1,10 @@
-import { Day } from "./Type"
+import { Services } from "./Services";
+import { Day, User } from "./Type"
 
 export type Employees = {
+  _id: string,
+  user: User,
+  serviceIds: Services[];
 }
 
 export type Schedule = {
